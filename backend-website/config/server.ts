94d8acfe82a3ec40ac97922ed1d506fc19fcd251
AxelResendizRzm <ivanramres@gmail.com>
2026-07-07 22:40:45 -0600

    add: domain

--- backend-website/config/server.ts
+++ backend-website/config/server.ts
@@ -3,7 +3,7 @@ import type { Core } from '@strapi/strapi';
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'https://conoce-nr-website-production.up.railway.app'), // <-- ESTO ES CRÍTICO
+  url: env('PUBLIC_URL', 'https://conoce-nr-website-production.up.railway.app'),
   app: {
     keys: env.array('APP_KEYS'),
   },
