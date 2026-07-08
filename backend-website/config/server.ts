83ef57583f4fbce23bc93851111e26134a735273
AxelResendizRzm <ivanramres@gmail.com>
2026-07-07 22:15:39 -0600

    add: domain

--- backend-website/config/server.ts
+++ backend-website/config/server.ts
@@ -1,11 +1,13 @@
 import type { Core } from '@strapi/strapi';
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Server => ({
+export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://conoce-nr-website-production.up.railway.app'), // <-- ESTO ES CRÍTICO
   app: {
     keys: env.array('APP_KEYS'),
   },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
 });
-
-export default config;
